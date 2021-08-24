ea9e13d3af5df16b58147b825327da90985efe63
Lionel Herbet <lionel.herbet@gmail.com>
2021-08-24 18:10:09 +0200

    move to docker

--- hack/workspace_env.sh
+++ hack/workspace_env.sh
@@ -3,8 +3,6 @@ set -o errexit
 set -o nounset
 
 cat <<EOF
-STABLE_PROJECT_NAME yad
-STABLE_PROJECT_NAMESPACE y0psolo
-REGISTRY quay.io
-BRANCH test
+STABLE_PROJECT_NAMESPACE yadist
+REGISTRY docker.io
 EOF
