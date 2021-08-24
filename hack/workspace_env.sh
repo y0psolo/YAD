c766b715e6122e5a3b5a7e017331e22b30c59d0b
Lionel Herbet <lionel.herbet@gmail.com>
2021-08-24 17:09:02 +0200

    publish test and shceduled workflow for updating

--- hack/workspace_env.sh
+++ hack/workspace_env.sh
@@ -3,8 +3,8 @@ set -o errexit
 set -o nounset
 
 cat <<EOF
-STABLE_PROJECT_NAME test
-STABLE_PROJECT_NAMESPACE test
-REGISTRY docker.io
+STABLE_PROJECT_NAME y0psolo
+STABLE_PROJECT_NAMESPACE yad
+REGISTRY quay.io
 BRANCH test
 EOF
