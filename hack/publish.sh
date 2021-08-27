d2eadfc4aed2b2bee17b4e556ee34f4db9cb4f8c
Lionel Herbet <lionel.herbet@gmail.com>
2021-08-27 11:30:10 +0200

    more test with buildah

--- hack/publish.sh
+++ hack/publish.sh
@@ -6,7 +6,7 @@ REGISTRY=$1
 
 . $SCRIPTPATH/common.sh
 
-pull_images_to_podman() {
+pull_images_to_buildah() {
     _suffix=""
     if [ $2 != 'latest' ]
     then
@@ -14,7 +14,7 @@ pull_images_to_podman() {
     fi
     for arch in amd64 arm64
     do
-        podman pull -q docker-daemon:${REPO}/$1:${arch}$_suffix
+        buildah pull -q docker-daemon:${REPO}/$1:${arch}$_suffix
     done
 }
 
@@ -24,11 +24,12 @@ merge_tag_arch() {
     then
         _suffix="_$2"
     fi
-    docker manifest create ${REPO}/$1:$2 ${REPO}/$1:amd64$_suffix ${REPO}/$1:arm64$_suffix
+    buildah manifest create localhost/${REPO}/$1:$2 localhost/${REPO}/$1:amd64$_suffix
+    buildah manifest add localhost/${REPO}/$1:$2 localhost/${REPO}/$1:arm64$_suffix
 }
 
 publish_image() {
-    docker push -q ${REPO}/$1:$2 ${REGISTRY}/${REPO}/$1:$2
+    buildah push -q --format docker localhost/${REPO}/$1:$2 docker://${REGISTRY}/${REPO}/$1:$2
 }
 
 delete_images_from_podman() {
@@ -44,9 +45,8 @@ delete_images_from_podman() {
     done
 }
 
-# loop_over_image_tag pull_images_to_podman
-# podman images list
-docker images list
+loop_over_image_tag pull_images_to_buildah
+buildah info
 loop_over_image_tag merge_tag_arch
-loop_over_image_tag publish_image
+# loop_over_image_tag publish_image
 # loop_over_image_tag delete_images_from_podman
