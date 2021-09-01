9d30dae3cffe47b1ba3fc37c7002901d446b164d
Lionel Herbet <lionel.herbet@gmail.com>
2021-09-01 10:39:56 +0200

    Fixed: python test failure Fixed: lds.so.cache update Fixed: ld_cache_gen.Sh script

--- layer/python/ld_cache_gen.sh
+++ layer/python/ld_cache_gen.sh
@@ -2,15 +2,15 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-bazel run //image/python:amd64_sh
-bazel run //image/python:arm64_sh
+bazel run //image/python:amd64
+bazel run //image/python:arm64
 
-docker run -u root --name pythonarm64 --entrypoint /bin/sh bazel/image/python:arm64_sh /sbin/ldconfig
+docker run -u root --name pythonarm64 --entrypoint /bin/sh bazel/image/python:arm64 /sbin/ldconfig
 docker cp pythonarm64:/etc/ld.so.cache $SCRIPTPATH/arm64/ld.so.cache
 docker rm pythonarm64
-docker rmi  bazel/image/python:arm64_sh
+docker rmi  bazel/image/python:arm64
 
-docker run -u root --name pythonamd64 --entrypoint /bin/sh bazel/image/python:amd64_sh /sbin/ldconfig
+docker run -u root --name pythonamd64 --entrypoint /bin/sh bazel/image/python:amd64 /sbin/ldconfig
 docker cp pythonamd64:/etc/ld.so.cache $SCRIPTPATH/amd64/ld.so.cache
 docker rm pythonamd64
-docker rmi  bazel/image/python:amd64_sh
+docker rmi  bazel/image/python:amd64
