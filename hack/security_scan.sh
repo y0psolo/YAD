e7cd550213277e0c428b5abf019b2b2a92c0a639
y0psolo <lionel.herbet@gmail.com>
2021-09-22 15:03:17 +0200

    replace html export by table export for trivy scan

--- hack/security_scan.sh
+++ hack/security_scan.sh
@@ -12,10 +12,12 @@ scan_image() {
         _suffix="_$2"
     fi
     docker run --rm -v /tmp/.cache/:/root/.cache/ -v $SCRIPTPATH/../security/sarif:/mnt/out/ aquasec/trivy --format template --template "@/contrib/sarif.tpl" -o /mnt/out/$1_$2.sarif ${REPO}/$1:$2
-    docker run --rm -v /tmp/.cache/:/root/.cache/ -v $SCRIPTPATH/../security/html:/mnt/out/ aquasec/trivy --format template --template "@/contrib/html.tpl" -o /mnt/out/$1_$2.html ${REPO}/$1:$2
+    # docker run --rm -v /tmp/.cache/:/root/.cache/ -v $SCRIPTPATH/../security/json:/mnt/out/ aquasec/trivy --format template --template "@/contrib/json.tpl" -o /mnt/out/$1_$2.json ${REPO}/$1:$2
+    docker run --rm -v /tmp/.cache/:/root/.cache/ -v $SCRIPTPATH/../security/table:/mnt/out/ aquasec/trivy --format table -o /mnt/out/$1_$2 ${REPO}/$1:$2
 }
 
 mkdir -p $SCRIPTPATH/../security/sarif
-mkdir -p $SCRIPTPATH/../security/html
+mkdir -p $SCRIPTPATH/../security/json
+mkdir -p $SCRIPTPATH/../security/table
 
 loop_over_image_tag scan_image
