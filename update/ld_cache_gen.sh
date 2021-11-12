fcdf36a5a6d607a9be32ae19fd7e53bc8fbd494a
Lionel Herbet <lionel.herbet@gmail.com>
2021-11-12 22:32:09 +0100

    fix update

--- update/ld_cache_gen.sh
+++ update/ld_cache_gen.sh
@@ -2,15 +2,15 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-bazel run //image/python:amd64
-bazel run //image/python:arm64
+bazel run --config x86_64 -c opt //image/python:python
+bazel run --config arm64 -c opt //image/python:python
 
-docker run -u root --name pythonarm64 --entrypoint /bin/sh bazel/image/python:arm64 /sbin/ldconfig
+docker run -u root --name pythonarm64 --entrypoint /bin/sh bazel/image/python:python /sbin/ldconfig
 docker cp pythonarm64:/etc/ld.so.cache $SCRIPTPATH/../layer/python/arm64/ld.so.cache
 docker rm pythonarm64
-docker rmi  bazel/image/python:arm64
+docker rmi  bazel/image/python:python
 
-docker run -u root --name pythonamd64 --entrypoint /bin/sh bazel/image/python:amd64 /sbin/ldconfig
+docker run -u root --name pythonamd64 --entrypoint /bin/sh bazel/image/python:python /sbin/ldconfig
 docker cp pythonamd64:/etc/ld.so.cache $SCRIPTPATH/../layer/python/amd64/ld.so.cache
 docker rm pythonamd64
-docker rmi  bazel/image/python:amd64
+docker rmi  bazel/image/python:python
