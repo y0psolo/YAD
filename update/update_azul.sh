8b8316113176710a8576dc6e98501b046737fdef
Lionel Herbet <lionel.herbet@gmail.com>
2022-01-18 14:52:02 +0100

    fix update azul script to rely on latest package

--- update/update_azul.sh
+++ update/update_azul.sh
@@ -7,10 +7,10 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 . $SCRIPTPATH/common.sh
 
 update_azul() {
-    PAYLOAD=$(curl -X 'GET' 'https://api.azul.com/zulu/download/community/v1.0/bundles/?java_version='$1'.0&os=linux&arch='$3'&hw_bitness=64&ext=tar.gz&bundle_type='$2'&javafx=false&release_status=ga&latest=true&support_term=lts' -H 'accept: application/json')
-    URL="$(echo $PAYLOAD | jq -r .[0].url)"
-    FILENAME=$(echo $PAYLOAD | jq -r .[0].name)
-    VERSION="$1.0.$(echo $PAYLOAD | jq -r .[0].java_version[2])"
+    PAYLOAD=$(curl -X 'GET' 'https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?java_version='$1'.0&os=linux&arch='$3'&hw_bitness=64&ext=tar.gz&bundle_type='$2'&javafx=false&release_status=ga&support_term=lts' -H 'accept: application/json')
+    URL="$(echo $PAYLOAD | jq -r .url)"
+    FILENAME=$(echo $PAYLOAD | jq -r .name)
+    VERSION="$1.0.$(echo $PAYLOAD | jq -r .java_version[2])"
     PREFIX="${FILENAME%.*.*}/$4"
     if [ $3 = "x86" ]
     then
