6f8ea3599a87fdb7ffdb4e2851cecdaa8717b1f1
Lionel Herbet <lionel.herbet@gmail.com>
2022-03-21 11:21:03 +0100

    add update postresql and mysql driver script

--- update/update_hive.sh
+++ update/update_hive.sh
@@ -12,3 +12,9 @@ VERSION=$(git -c versionsort.suffix=- ls-remote -t --exit-code --refs --sort=-v:
 write_rule hive_metastore_3 hive_metastore_3 https://repo1.maven.org/maven2/org/apache/hive/hive-standalone-metastore/$VERSION/hive-standalone-metastore-$VERSION-bin.tar.gz apache-hive-metastore-$VERSION-bin/ hive
 # Update update/constants.bzl file
 write_version hive  HIVE_VERSION $VERSION
+
+VERSION_PGSQL=$(git -c versionsort.suffix=- ls-remote -t --exit-code --refs --sort=-v:refname https://github.com/pgjdbc/pgjdbc 'REL[0-9]*.[0-9]*.[0-9]*' | grep -o -m 1 -E "[0-9]+\.[0-9]+\.[0-9]+$")
+write_rule hive_pgsql postgresql https://repo1.maven.org/maven2/org/postgresql/postgresql/$VERSION_PGSQL/postgresql-$VERSION_PGSQL.jar
+
+VERSION_MYSQL=$(git -c versionsort.suffix=- ls-remote -t --exit-code --refs --sort=-v:refname https://github.com/mysql/mysql-connector-j '[0-9]*.[0-9]*.[0-9]*' | grep -o -m 1 -E "[0-9]+\.[0-9]+\.[0-9]+$")
+write_rule hive_mysql mysql https://repo1.maven.org/maven2/mysql/mysql-connector-java/$VERSION_MYSQL/mysql-connector-java-$VERSION_MYSQL.jar
