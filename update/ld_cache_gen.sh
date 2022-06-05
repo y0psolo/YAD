b2a125b24ec001fea68aee6d9e75527b6ba139c0
Lionel Herbet <lionel.herbet@gmail.com>
2022-06-05 22:45:44 +0200

    fix ci, python bug, merge tag script and github worflow

--- update/ld_cache_gen.sh
+++ update/ld_cache_gen.sh
@@ -29,3 +29,17 @@ docker run -u root --name pythonamd64 --entrypoint /bin/sh bazel/image/python:py
 docker cp pythonamd64:/etc/ld.so.cache $SCRIPTPATH/../layer/python/amd64/20.04/ld.so.cache
 docker rm pythonamd64
 docker rmi  bazel/image/python:python
+
+mkdir -p $SCRIPTPATH/../layer/python/arm64/22.04/
+bazel run --config arm64 -c opt --distro=22.04 //image/python:python
+docker run -u root --name pythonarm64 --entrypoint /bin/sh bazel/image/python:python /sbin/ldconfig
+docker cp pythonarm64:/etc/ld.so.cache $SCRIPTPATH/../layer/python/arm64/22.04/ld.so.cache
+docker rm pythonarm64
+docker rmi  bazel/image/python:python
+
+mkdir -p $SCRIPTPATH/../layer/python/amd64/22.04/
+bazel run --config x86_64 -c opt --distro=22.04 //image/python:python
+docker run -u root --name pythonamd64 --entrypoint /bin/sh bazel/image/python:python /sbin/ldconfig
+docker cp pythonamd64:/etc/ld.so.cache $SCRIPTPATH/../layer/python/amd64/22.04/ld.so.cache
+docker rm pythonamd64
+docker rmi  bazel/image/python:python
